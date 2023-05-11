766d3ae2454b15570d6fa3b3654d295e6a1ded08
Lee Juin <juin.lee@outlook.com>
2023-05-11 14:39:10 +0800

    update DB credentials

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/users.module';
 import { CheckAuthMiddleware } from './users/user.middleware';
+import * as dotenv from "dotenv";
 
+dotenv.config();
 @Module({
-	imports: [MongooseModule.forRoot('mongodb+srv://neozenith:nlphub@nlp-hub.mbc3aja.mongodb.net/nlp-hub-db?retryWrites=true&w=majority'), UserModule],
+	imports: [MongooseModule.forRoot('mongodb+srv://neozenith:'+ process.env.DB_SECRET +'@nlp-hub.mbc3aja.mongodb.net/nlp-hub-db?retryWrites=true&w=majority'), UserModule],
 	controllers: [],
 	providers: [],
 })
