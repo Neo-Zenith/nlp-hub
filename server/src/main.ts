99f8c30a52016f70fc0ec6726f4a6e1211b77003
Lee Juin <juin.lee@outlook.com>
2023-05-23 00:01:51 +0800

    setup for cloud deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-  	await app.listen(3000);
+	const options = new DocumentBuilder()
+		.setTitle('NLP Hub')
+		.setDescription('API endpoints for NLP Hub')
+		.setVersion('1.0') 
+        .addBearerAuth(
+            { 
+              description: `Please enter your access token`,
+              name: 'Authorization',
+              bearerFormat: 'Bearer',
+              scheme: 'Bearer',
+              type: 'http', 
+              in: 'Header'
+            },
+            'access-token')
+        .build();
+
+	const document = SwaggerModule.createDocument(app, options);
+  	SwaggerModule.setup('api', app, document);
+  	await app.listen(8080);
 }
 
 bootstrap();
