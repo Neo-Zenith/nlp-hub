a714f033edeb7a1a58a1ff205ad443f97ea07f27
Lee Juin <juin.lee@outlook.com>
2023-06-05 23:37:39 +0800

    login and signup page styling

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { LoginComponent } from "../components/CredentialsForm";
+import "../styles/pages/CredentialsPage.css";
 
 export function LoginPage() {
-    return <LoginComponent />;
+    return (
+        <>
+            <div className="login-page-wrapper">
+                <LoginComponent className="login-component" />
+            </div>
+        </>
+    );
 }
--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { SignupComponent } from "../components/CredentialsForm";
+import "../styles/pages/CredentialsPage.css";
 
 export function SignupPage() {
-    return <SignupComponent />;
+    return (
+        <>
+            <div className="signup-page-wrapper">
+                <SignupComponent className="signup-component" />
+            </div>
+        </>
+    );
 }
